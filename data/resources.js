c80cd7ca86ff1539562703a32df4740d07071b0c
Matt Lane <lane.matthew@gmail.com>
2016-07-01 21:12:09 -0700

    add resources data

---
+++ data/resources.js
@@ -0,0 +1,264 @@
+module.exports = [
+  {
+    resource: 'Baby Photography'
+    options: [
+      {
+        name: 'Martina Smolova - B.A.N.D member',
+        phone: '925 330 5509'
+        url: 'http://www.martinasmolovaphotography.com'
+      }, {
+        name: 'Danielle Elizabeth',
+        phone: '401 368 0317',
+        url: 'http://danielleelizabethphoto.blogspot.com'
+      }
+    ]
+  }, {
+    resource: 'Bereaved Family Support',
+    options: [
+      {
+        name: 'Hand on the Peninsula',
+        phone: '650 367 6993',
+        url: 'http://www.handsupport.org'
+      }
+    ]
+  }, {
+    resource: 'Birth Doulas/Midwives',
+    options: [
+      {
+        name: 'San Francisco Doula Group',
+        phone: '',
+        url: 'http://www.sfdoulagroup.com/birthdoulas.html'
+      }, {
+        name: 'Bay Area Home Birth',
+        phone: '',
+        url: 'http://www.bayareahomebirth.org'
+      }
+    ]
+  }, {
+    resource: 'Breastfeeding Support',
+    options: [
+      {
+        name: 'Gillian Croke, IBCLC - B.A.N.D. member',
+        phone: '415 823 3774',
+        url: ''
+      }, {
+        name: 'La Leche League',
+        phone: '',
+        url: 'http://www.lalecheleague.org'
+      }, {
+        name: 'WIC Breastfeeding Help Line',
+        phone: '1 800 205 0333',
+        url: ''
+      }
+    ]
+  }, {
+    resource: 'Chiropractor',
+    options: [
+      {
+        name: 'Laura Sheehan',
+        phone: '415 681 1031',
+        url: 'http://www.laurasheehan.com'
+      }
+    ]
+  }, {
+    resource: 'Families with Disabilities',
+    options: [
+      {
+        name: 'Looking Glass',
+        phone: '',
+        url: 'http://www.lookingglass.org'
+      }
+    ]
+  }, {
+    resource: 'Giving Back',
+    options: [
+      {
+        name: 'Help a Mother Out',
+        phone: '',
+        url: 'http://www.helpamotherout.org'
+      }, {
+        name: 'Homeless Prenatal Program',
+        phone: '415 546 6756 ext. 381',
+        url: ''
+      }
+    ]
+  }, {
+    resource: 'Hypnobirthing',
+    options: [
+      {
+        name: 'Kathy Woo',
+        phone: '',
+        url: 'http://www.kathywoo.com'
+      }, {
+        name: 'Rachel Yellin',
+        phone: '',
+        url: 'http://www.rachelyellin.com'
+      }
+    ]
+  }, {
+    resource: 'Multiple Birth Groups',
+    options: [
+      {
+        name: 'San Francisco Parents of Multiples',
+        phone: '',
+        url: 'http://www.sfpom.org'
+      }, {
+        name: 'Twins by the Bay Group',
+        phone: '',
+        url: 'http://www.twinsbythebay.org'
+      }, {
+        name: 'Marin Parents of Multiples Club',
+        phone: '',
+        url: 'http://www.mpomc.org'
+      }, {
+        name: 'Mid Peninsula Parents of Multiples',
+        phone: '',
+        url: 'http://www.mppom.org'
+      }
+    ]
+  }, {
+    resource: 'Newborn Resources and Education',
+    options: [
+      {
+        name: 'Day One Baby', 
+        phone: '',
+        url: 'http://www.dayonebaby.com'
+      }
+    ]
+  }, {
+    resource: 'Parent Groups',
+    options: [
+      {
+        name: 'Golden Gate Mother\'s Group',
+        phone: '',
+        url: 'http://www.ggmg.com'
+      }, {
+        name: 'East Bay Mom\'s Group'
+        phone: '',
+        url: 'http://www.eastbaymoms.com'
+      }, {
+        name: 'Marin Mommies Group',
+        phone: '',
+        url: 'http://www.marinmommies.com'
+      }, {
+        name: 'San Mateo Mother\'s Club',
+        phone: '',
+        url: 'http://www.sanmateomothersclub.org'
+      }, {
+        name: 'Single Parent Groups',
+        phone: '',
+        url: 'http://www.meetup.com'
+      }, {
+        name: 'The Bay Area Gay and Lesbian Group',
+        phone: '',
+        url: 'http://www.ourfamily.org'
+      }
+    ]
+  }, {
+    resource: 'Postpartum Support',
+    options: [
+      {
+        name: 'Mama Tong Soups',
+        phone: '',
+        url: 'http://mamatong.blogspot.com'
+      }, {
+        name: 'Mama Kai',
+        phone: '',
+        url: 'http://www.mamakai.org' 
+      }, {
+        name: 'Mrs. Patel\'s, for Nursing Mothers',
+        phone: '',
+        url: 'http://www.mrsmilk.com'
+      }, {
+        name: 'CPMC - Newborn Connections',
+        phone: '',
+        url: 'http://www.cpmc.org/services/pregnancy/information/postpartum-emotions.html'
+      }, {
+        name: 'Dr. Shoshana Bennett',
+        phone: '',
+        url: 'http://www.drshosh.com'
+      }
+    ]
+  }, {
+    resource: 'Preemie Support',
+    options: [
+      {
+        name: 'The Graham Foundation',
+        phone: '',
+        url: 'http://www.grahamfoundation.org'
+      }
+    ]
+  }, {
+    resource: 'Pregnancy, Parent Support, and Classes',
+    options: [
+      {
+        name: 'Day One Center',
+        phone: '415 440 3291 (San Francisco) / 650 322 3291 (Palo Alto)'
+        url: 'http://www.dayonecenter.com'
+      }, {
+        name: 'Urban Recess',
+        phone: '',
+        url: 'http://www.recessurbanrecreation.com'
+      }, {
+        name: 'Natural Resources', 
+        phone: '415 550 2611',
+        url: 'http://www.naturalresources-sf.com'
+      }, {
+        name: 'Blossom Birth',
+        phone: '650 321 2326',
+        url: 'http://www.blossombirth.org'
+      }, {
+        name: 'Birthways', 
+        phone: '510 869 2797',
+        url: 'http://www.birthways.org'
+      }
+    ]
+  }, {
+    resource: 'Sleep Specialists',
+    options: [
+      {
+        name: 'Angelique Millette',
+        phone: '',
+        url: 'http://www.angeliquemillette.com'
+      }, {
+        name: 'Vivian Sonnenberg',
+        phone: '',
+        url: 'http://www.viviansonnenberg.com'
+      }
+    ]
+  }, {
+    resource: 'Suggested Reading',
+    options: [
+      {
+        name: 'The Womanly Art of Breastfeeding - La Leche League International',
+        phone: '',
+        url: ''
+      }, {
+        name: 'Mothering Multiples - La Leche League International',
+        phone: '',
+        url: ''
+      }, {
+        name: 'The Baby Book - Dr. Sears',
+        phone: '',
+        url: ''
+      }, {
+        name: 'The Happiest Baby on the Block - Dr. Harvey Karp',
+        phone: '',
+        url: ''
+      }
+    ]
+  }, {
+    resource: 'Yoga Classes',
+    options: [
+      {
+        name: 'The Mindful Body',
+        phone: '',
+        url: 'http://www.mindfulbody.com'
+      }, {
+        name: 'Yogalayam',
+        phone: '',
+        url: 'http://www.yogalayam.org'
+      }
+    ]
+  }
+]
